7acfd4b65f19a15bbb8eb5672eb2912ac99b11f5
Ravee Kirann <raveekirann@Ravee-Kiranns-Mac-2.local>
2019-11-14 18:40:06 +0530

    fix .travis file 7

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t raveekirann/multi-client:latest -t raveekirann/multi-client:$SHA -f ./client/Dockerfile ./client
-doeckr build -t raveekirann/multi-server:latest -t raveekirann/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t raveekirann/multi-server:latest -t raveekirann/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t raveekirann/multi-worker:latest -t raveekirann/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push raveekirann/multi-client:latest
 docker push raveekirann/multi-server:latest
 docker push raveekirann/multi-worker:latest
@@ -8,7 +9,8 @@ docker push raveekirann/multi-worker:latest
 docker push raveekirann/multi-client:$SHA
 docker push raveekirann/multi-server:$SHA
 docker push raveekirann/multi-worker:$SHA
-kubectl apply k8s
+
+kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=raveekirann/multi-server:$SHA
-kubectl set image deployments/client-deployment server=raveekirann/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=raveekirann/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=raveekirann/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=raveekirann/multi-worker:$SHA
